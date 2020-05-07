13d98e9f2e2c1321e531de34d51aa33a1dddc468
Justin Greever <jgreever@gmail.com>
2020-05-07 15:32:58 -0700

    Practice, Random sized, random number, linked list created and working.

--- Practice/LLL/node.cpp
+++ Practice/LLL/node.cpp
@@ -39,8 +39,9 @@ node::~node()
 void display_all(node * head)
 {
     if(!head) return;
+    cout << head->data << " <- "; //output in reverse order
     display_all(head->next);
-    cout << head->data << endl;
+    //cout << head->data << " -> "; //output in normal order
 }
 
 void push(node ** head, int some_data)
@@ -51,36 +52,53 @@ void push(node ** head, int some_data)
     (*head) = temp;
 }
 
+void gen_random_list(node ** head)
+{
+    srand(time(0));
+    node * temp = new node();
+    temp->data = rand() % 100;
+    temp->next = (*head);
+    (*head) = temp;
+}
 int main()
 {
-    /*
     //create a list and display back in order
     node * head = NULL;
-    int some_data = 0;
-    int flag = -1;
-    while(flag != 2)
-    {
-    cout << "\nPlease type any number from 1 to 100: ";
-    cin >> some_data;
-    cin.ignore(100, '\n'); 
-    push(&head, some_data);
-    cout << "\nEnter another? 1 for Yes,  or 2 for No: ";
-    cin >> flag;
-    cin.ignore(100, '\n');
-    }
-    */
+    //int some_data = 0;
+    //int flag = -1;
+    //while(flag != 2)
+    //{
+        //cout << "\nPlease type any number from 1 to 100: ";
+        //cin >> some_data;
+        //cin.ignore(100, '\n'); 
+        cout << "Generating a random list of numbers of random size..." << endl;
+        srand(time(0));
+        int node_random = rand() % 25;
+        //some_data = rand() % 100;
+        for(int i = 0; i < node_random; i++)
+        {
+            int some_data = rand() % 25;
+            push(&head, some_data);
+        }
+        cout << "Done, Random list of random size has been generated." << endl;
+        //cout << "\nEnter another? 1 for Yes,  or 2 for No: ";
+        //cin >> flag;
+        //cin.ignore(100, '\n');
+    
 
     //create a list and insert random numbers for
     //the next practice problem(s)
-    node * temp = new node(); temp = NULL;
-    for(int i = 0; i < 20; i++)
-    {
-        srand(time(0));
-        int random = 0;
-        random = rand() % 100;
-        push(&temp, random);
-    }
-
-    display_all(temp);
+    /*
+     * not working correctly yet
+     node * temp = new node(); temp = NULL;
+     for(int i = 0; i < 20; i++)
+     {
+     srand(time(0));
+     int random = 0;
+     random = rand() % 100;
+     push(&temp, random);
+     }
+     */
+    display_all(head);
     return 0;
 }
